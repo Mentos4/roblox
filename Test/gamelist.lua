0ab0d69c708d4ec5914f4e0f4a17fc25b10a5cc7
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-05-27 07:05:10 +0800

    Update gamelist.lua

--- Test/gamelist.lua
+++ Test/gamelist.lua
@@ -3,6 +3,8 @@ local Games = {
     ["9529182643"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Test/test/Script_Levelbound.lua",
     -- Garden Horizon
     ["9509842868"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Test/test/GardenHorizon",
+    -- Bee Garden
+    ["8220767002"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Bee%20Garden",
 }
 
 
