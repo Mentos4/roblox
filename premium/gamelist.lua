0de67ec87369f127b6760d1dcc383c6ea4ef7fe7
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-05-31 19:21:54 +0800

    Update gamelist.lua

--- premium/gamelist.lua
+++ premium/gamelist.lua
@@ -52,7 +52,7 @@ local Games = {
     -- The Forge
     ["7671049560"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/premium/Script/TheForgeFinal.lua",
     -- Wizard Alchemy
-    ["118821269826806"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/premium/Script/Wizard%20Alchemy",
+    ["10006104044"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/premium/Script/Wizard%20Alchemy",
 }
 
 return Games
