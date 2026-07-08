840a9619d015e8f8f2d722bd70cc4dfae75db701
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-07-09 02:03:35 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,4 +1,6 @@
 local Games = {
+    -- +1 Mine Per Click
+    ["10178802449"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/%2B1%20Mine%20Per%20Click",
     -- Anime Eternal
     ["7882829745"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Script_AnimeEternal.lua",
     -- Anime Fight
