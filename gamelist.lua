3cfe3533b4a5284d16c75716ed7b765a47618c04
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-08-09 01:41:08 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -67,6 +67,8 @@ local Games = {
     ["7671049560"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Script_TheForge.lua",
     -- Wizard Alchemy
     ["10006104044"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Wizard%20Alchemy",
+    -- World Zero
+    ["985731078"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/World%20Zero",
 }
 
 return Games
