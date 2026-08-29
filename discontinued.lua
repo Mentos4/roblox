5ac67a1c7b856012035fbaac1aba1e61b1bf3980
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-08-30 01:30:49 +0800

    Update discontinued.lua

--- discontinued.lua
+++ discontinued.lua
@@ -1,11 +1,15 @@
 -- List of discontinued game IDs
 local Discontinued = {
     ["5995470825"] = true, -- Hypershot
-    ["6701277882"] = true, -- Fish it
+    ["6701277882"] = true, -- Fish It
     ["6867859535"] = true, -- RE:XL
     ["7074860883"] = true, -- Arise Crossover
     ["8820222330"] = true, -- Dig and Hatch a Brainrot
-    -- Add more discontinued game IDs here
+    ["9073775318"] = true, -- Slime Seas
+    ["9792947201"] = true, -- Slime RNG
+    ["9802644580"] = true, -- Summon Heroes
+    ["9529182643"] = true, -- Levelbound (Premium Only)
+    ["8328640632"] = true, -- Farm It
 }
 
 return Discontinued
