3d95091342db5461b51e73e4ef7ccc76ddffdfbf
Mentos4 <zandrixjanpongos0104@gmail.com>
2026-09-06 01:42:26 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -72,7 +72,7 @@ local Games = {
     -- Swordburst 3
     ["4093155512"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Script_Swordburst3.lua",
     -- Steal an Egg
-    ["10563114921"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Steal%20an%20Egg",
+    ["105631149211"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/Steal%20an%20Egg",
     -- The Forge
     ["7671049560"] = "https://raw.githubusercontent.com/Mentos4/roblox/refs/heads/main/Script/The%20Forge",
     -- Wizard Alchemy
